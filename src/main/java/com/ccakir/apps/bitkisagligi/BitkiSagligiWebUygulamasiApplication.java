f89b9d96065b9e37590973030673509f764def64
mkutlu <mkutlu13@gmail.com>
2021-07-19 19:49:53 +0300

    Initial commit.

---
+++ src/main/java/com/ccakir/apps/bitkisagligi/BitkiSagligiWebUygulamasiApplication.java
@@ -0,0 +1,13 @@
+package com.ccakir.apps.bitkisagligi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitkiSagligiWebUygulamasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitkiSagligiWebUygulamasiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ccakir/apps/bitkisagligi/BitkiSagligiWebUygulamasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccakir.apps.bitkisagligi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitkiSagligiWebUygulamasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
